2a3bd90df37600c06958e5cc50e24a2e614507f7
calmquant <calmquant@gmail.com>
2022-05-04 20:41:55 +0200

    WIP update notion utils add some notebooks for scratch start working with opensource notion sdk

--- dev/notion_api.py
+++ dev/notion_api.py
@@ -5,12 +5,20 @@ Without fancy features and extensions, e.g. autocompletion
 
 from enum import Enum
 import requests
+import json
 
 
-# class RequestType(Enum):
+class RequestType(Enum):
     post = "POST"
     get = "GET"
 
 
-def send_request(request_type: RequestType, ):
+class ApiHandle(Enum):
+    database = "databases"
+
+
+def send_notion_request(request_type: RequestType, api_handle: ApiHandle, item_id: str, headers, data: dict = None):
     pass
+    url = f"https://api.notion.com/v1/{api_handle.value}/{item_id}"
+    res = requests.request(request_type.value, url, headers=headers, data=json.dumps(data)).json()
+    return res
