13ee3bb0d1f99cab4088d694b75be6c8b0f9dd82
calmquant <calmquant@gmail.com>
2022-06-16 05:07:56 +0200

    Jarvis_3.ipynb:   Notion generators - methods and commands for adding items     todo, task     idea     todo: recommendation   Notion tags - methods and commands for modifying items metadata     draft: link, expose_to     draft: who, source     draft: generic tag, metadata     todo: pre-process generator tags (#idea etc) - required for simple text support     todo: support enums
      class Jarvis:
        init
        generators
        tags
        run
    
      utils:
        compose_item
        compose_block
    
    notion_lib.py:
      Notion
      NotionDB
      NotionPage
      NotionObject (parent class)
    
    notion_glow.py:
      Add fancy page search draft - to resolve issues with too many pages: filter out archived pages etc.
    
    todo: add required fancies. What's already ready and easy to do:
    - reply to user with informative messages (page link)
    - help message if there's confusion (list supported commands, docstrings)
    - parse simple text messages properly (to be re-used for inline parsing as well)
    - proper page search for #link and #who (db_people for #who)
    
    Tech debt:
    - Logging!
    - Catch errors, don't crash
    
    Essential features to follow-up:
    - quick lookup - people
    - auto-maps: people, projects, dbs, contexts (recent, big)
    - page sorting, info request

--- dev/notion_glow.py
+++ dev/notion_glow.py
@@ -11,3 +11,17 @@ And core item types
 
     - Working session
 """
+from dev.notion_lib import Notion
+
+
+class GlowNotion(Notion):
+
+    def find_page(self, query, context=None):
+        pass
+        # todo: support shortcuts
+        # todo: narrow search to main item types
+
+        # todo: fancy dynamic heuristics
+            # dynamically expand/narrow search based on # of results
+
+            # request additional info from user via telegram
