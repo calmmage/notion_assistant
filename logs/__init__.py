117f4bda42c3abd616cb8f1f1d77444b05f28a08
kudrinsky <kudrinsky.am@gmail.com>
2022-07-17 20:30:32 +0300

    logger in logs

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,1 +1,11 @@
 from .setup_logging import setup_logger
+
+import json 
+import logging
+
+logging.basicConfig(level=logging.NOTSET)
+
+with open('secrets.json') as secrets_file:
+    secrets  = json.load(secrets_file)
+
+LOGGER = setup_logger(secrets)
