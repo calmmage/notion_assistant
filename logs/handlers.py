e4b5e24c418991f86816be644deeb03df91a03d3
kudrinsky <kudrinsky.am@gmail.com>
2022-07-17 19:16:39 +0300

    notion handler v1

--- logs/handlers.py
+++ logs/handlers.py
@@ -1,10 +1,80 @@
 from logging import StreamHandler, FileHandler
 import logging
 
+from notion_client import Client
+
+
+class NotionLoggingHandler(StreamHandler):
+    def __init__(self, notion_database_id, notion_secret_key):
+        super().__init__()
+        self.notion_client = Client(auth=notion_secret_key)
+        self.notion_database_id = notion_database_id
+        
+
+    def emit(self, record: logging.LogRecord):
+        if 'logging_parsing_result' in record.args:
+            self._log_parsing_result(record)
+        else:
+            self._log_ordinary_result(record)
+
+
+    def _log_parsing_result(self, record: logging.LogRecord):
+        self.client.pages.create(
+            parent={"database_id": self.notion_database_id},
+            properties=NotionLoggingHandler._parse_message_to_page(record),
+            children=[
+                {
+                    "object": 'block',
+                    "type": 'paragraph',
+                    "paragraph": {
+                        "rich_text": [
+                            {
+                                "type": 'text',
+                                "text": {
+                                    "content": record.args['logging_parsing_result'],
+                                },
+                            },
+                        ],
+                    },
+                },
+            ]
+        )
+
+    def _log_ordinary_result(self, record: logging.LogRecord):
+        self.client.pages.create(
+            parent={"database_id": self.notion_database_id},
+            properties=NotionLoggingHandler._parse_message_to_page(record)
+        )
+
+    @staticmethod
+    def _parse_message_to_page(record: logging.LogRecord):
+        return {
+                "level": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": record.level,
+                            },
+                        },
+                    ],
+                },
+                "message": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": record.getMessage(),
+                            },
+                        },
+                    ],
+                },
+            }
+
 def get_all_log_handlers(
+        notion_database_id,
+        notion_secret_key,
         strfmt = '[%(asctime)s] [%(name)s] [%(levelname)s] > %(message)s',
         datefmt = '%Y-%m-%d %H:%M:%S',
-        logfile_path = 'logs.txt'
+        logfile_path = 'logs.txt',
 ):
     formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
 
@@ -16,9 +86,14 @@ def get_all_log_handlers(
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
+    notion_handler = NotionLoggingHandler(notion_database_id, notion_secret_key)
+    notion_handler.setLevel(logging.DEBUG)
+    notion_handler.setFormatter(formatter)
+
     handlers = [
         stream_handler,
-        file_handler
+        file_handler,
+        notion_handler
     ]
 
     return handlers
