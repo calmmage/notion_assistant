eef8de80d2f466193c5f1504659bdfa8a2685ac4
kudrinsky <kudrinsky.am@gmail.com>
2022-07-17 19:26:28 +0300

    logs

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
-from logs import setup_logger
-import scratch
+import json 
 
-setup_logger()
+from logs import setup_logger
+import scratch.NA_mvp
+
+with open('secrets.json') as secrets_file:
+    secrets  = json.load(secrets_file)
+
+setup_logger(secrets)
--- logs/setup_logging.py
+++ logs/setup_logging.py
@@ -2,9 +2,9 @@ import logging
 from handlers import get_all_log_handlers
 
 
-def setup_logger() -> logging.Logger:
+def setup_logger(secrets) -> logging.Logger:
     """https://docs.python.org/3/howto/logging.html#advanced-logging-tutorial"""
 
-    handlers = get_all_log_handlers()
+    handlers = get_all_log_handlers(secrets['notion_log_db_id'], secrets['notion_token'])
     for handler in handlers:
         logging.getLogger('').addHandler(handler) # TODO: adding directly to root loggger may be unsafe?
